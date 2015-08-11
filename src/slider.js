c9c41d3c5bba33bd231b731472a80d6601e4edb9
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2015-08-12 01:18:34 +0200

    Slider handle

--- src/slider.js
+++ src/slider.js
@@ -1,12 +1,19 @@
 import {select} from "d3-selection";  // https://github.com/d3/d3-selection
-//import {linear} from "d3-scale";      // https://github.com/d3/d3-scale
+import {format} from "d3-format";     // https://github.com/d3/d3-format
+//import {linear} from "d3-scale";    // https://github.com/d3/d3-scale
 
 export default function () {
+
+  // Public variables width default settings
   var min = 0,
       max = 100,
       orientation = "horizontal",
       value,
       scale;
+
+  // Private variables
+  var formatPercent = format(".2%"),
+      handle;
 
   function slider(selection) {
     selection.each(function() {
@@ -22,12 +29,33 @@ export default function () {
       // DIV container
       var div = select(this).class("d3-slider d3-slider-" + orientation, true);
 
+      // Slider handle
+      handle = div.append("a")
+          .class("d3-slider-handle", true)
+          .attr("xlink:href", "#");
+
+      // Horizontal slider
+      if (orientation === "horizontal") {
+        moveHandle(value);
+      }
+
     });
+  }
+
+  function moveHandle(newValue) {
+    var position = (orientation === "horizontal") ? "left" : "bottom";
+    handle.style(position, formatPercent(scale(newValue)));
   }
 
   slider.scale = function(_) {
     if (!arguments.length) return scale;
     scale = _;
+    return slider;
+  };
+
+  slider.value = function(_) {
+    if (!arguments.length) return value;
+    value = _;
     return slider;
   };
 
